c5d9161ede90141427ee966b7739a1b3c8675e51
nerpz <connerhicks53@gmail.com>
2021-06-15 13:57:34 -0400

    initial push

---
+++ app.js
@@ -0,0 +1,17 @@
+const postList = document.getElementById('posts');
+
+fetch('http://one-cms.herokuapp.com/api/public/read?api-key=connerhicks')
+  .then((response) => response.json())
+  .then((posts) => gotPosts(posts));
+
+const gotPosts = async (posts) => {
+  posts.forEach((post, i) => {
+    let date = new Date(post.createdAt);
+    let time = date.toLocaleTimeString();
+    postList.innerHTML += `<li>${post.title}(${time}): ${post.body}</li>`;
+
+    if (i === posts.length) {
+      insertNow(postInsert);
+    }
+  });
+};
