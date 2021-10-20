83507c29350ba5527b924011a079bcdfbb5fcbb2
nerpz <connerhicks53@gmail.com>
2021-10-20 07:40:48 -0400

    image map

--- frontend/src/pages/Gallery.js
+++ frontend/src/pages/Gallery.js
@@ -1,60 +1,62 @@
-const ImageFrame = (props) => {
+const imageURLs = [
+	'/images/gallery/SkateboardingEgg.svg',
+	'/images/gallery/PoeSanity.svg',
 
-}
+	'/images/gallery/Windows.svg',
+	'/images/gallery/LadyMaskGhanna.svg',
+	'/images/gallery/Squiggs.svg',
+	'/images/gallery/Allatoona.svg',
+	'/images/gallery/Diamonds.svg',
+	'/images/gallery/Network.svg',
+];
 
 const GalleryImage = (props) => {
-   const {mainColor, imgUrl, imgAlt} = props;
+	const { mainColor, imgUrl, imgAlt } = props;
 
-   const colorPicker = () => {
-      if(mainColor === '#053') {
-         return {backgroundColor: '#08f'}
-      } else if(mainColor === '#08f') {
-         return {backgroundColor: 'yellow'}
-      } else if(mainColor === 'yellow') {
-         return {backgroundColor: '#08f'}
-      } else if(mainColor === 'red') {
-         return {backgroundColor: '#053'}
-      } else return ``
-   }
-   return <div className='galleryFrame' style={colorPicker()}>
-      <img className='galleryImage' src={imgUrl} alt={imgAlt}/>
-   </div>
-}
+	const colorPicker = () => {
+		if (mainColor === '#053') {
+			return { backgroundColor: '#08f' };
+		} else if (mainColor === '#08f') {
+			return { backgroundColor: 'yellow' };
+		} else if (mainColor === 'yellow') {
+			return { backgroundColor: '#08f' };
+		} else if (mainColor === 'red') {
+			return { backgroundColor: '#053' };
+		} else return ``;
+	};
+	return (
+		<div className='galleryFrame' style={colorPicker()}>
+			<img className='galleryImage' src={imgUrl} alt={imgAlt} />
+		</div>
+	);
+};
 
 const Gallery = (props) => {
-   const {book, mainColor} = props;
-   const {activePage, oldPage} = book;
-   const colorPicker = () => {
-      if(mainColor === '#053') {
-         return {backgroundColor: '#08f'}
-      } else if(mainColor === '#08f') {
-         return {backgroundColor: 'yellow'}
-      } else if(mainColor === 'yellow') {
-         return {backgroundColor: '#08f'}
-      } else if(mainColor === 'red') {
-         return {backgroundColor: '#053'}
-      } else return ``
-   }
+	const { book, mainColor } = props;
+	const { activePage, oldPage } = book;
+	const colorPicker = () => {
+		if (mainColor === '#053') {
+			return { backgroundColor: '#08f' };
+		} else if (mainColor === '#08f') {
+			return { backgroundColor: 'yellow' };
+		} else if (mainColor === 'yellow') {
+			return { backgroundColor: '#08f' };
+		} else if (mainColor === 'red') {
+			return { backgroundColor: '#053' };
+		} else return ``;
+	};
 
-   return (
-      <main className="main">
-         <h1 className="title">Gallery</h1>
-       
-         <div className="gallery">
-            <GalleryImage mainColor={mainColor} imgUrl='/images/gallery/SkateboardingEgg.svg'/>
-            <GalleryImage mainColor={mainColor} imgUrl='/images/gallery/Windows.svg'/>
-            <GalleryImage mainColor={mainColor} imgUrl='/images/gallery/LadyMaskGhanna.svg'/>
-            <GalleryImage mainColor={mainColor} imgUrl='/images/gallery/Squiggs.svg'/>
-            <GalleryImage mainColor={mainColor} imgUrl='/images/gallery/Allatoona.svg'/>
-            <GalleryImage mainColor={mainColor} imgUrl='/images/gallery/Diamonds.svg'/>
-            <GalleryImage mainColor={mainColor} imgUrl='/images/gallery/Network.svg'/>
-            {/* <GalleryImage mainColor={mainColor} imgUrl='/images/gallery/'/> */}
-            {/* <GalleryImage mainColor={mainColor} imgUrl='https://nerblog.s3.amazonaws.com/.png'/> */}
-           
-         </div>
-         
-      </main>
-   )
-}
+	return (
+		<main className='main'>
+			<h1 className='title'>Gallery</h1>
 
-export default Gallery
+			<div className='gallery'>
+				{imageURLs.map((url) => (
+					<GalleryImage mainColor={mainColor} imgUrl={url} />
+				))}
+			</div>
+		</main>
+	);
+};
+
+export default Gallery;
